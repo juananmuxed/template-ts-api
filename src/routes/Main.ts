19135018552ec284b7c518c6972019ed8c21a3e0
MuXeD <juanan.sm87@gmail.com>
2023-12-16 16:08:41 +0100

    ✨ Added roles & users endpoints

--- src/routes/Main.ts
+++ src/routes/Main.ts
@@ -1,9 +1,13 @@
 import { Application } from 'express';
 
+import usersRoutes from '@routes/users/Users';
+import rolesRoutes from '@routes/users/Roles';
 import authenticationRoutes from '@routes/auth/Auth';
 
 const Paths = [
   'docs',
+  'users',
+  'roles',
   'authentication',
 ] as const;
 
@@ -12,9 +16,13 @@ type ApiPaths = typeof Paths[number];
 const rootPath = '/api/';
 export const apiPaths: Record<ApiPaths, string> = {
   docs: `${rootPath}docs`,
+  users: `${rootPath}users`,
+  roles: `${rootPath}roles`,
   authentication: `${rootPath}auth`,
 };
 
 export const setRoutes = (app: Application) => {
+  app.use(apiPaths.users, usersRoutes);
+  app.use(apiPaths.roles, rolesRoutes);
   app.use(apiPaths.authentication, authenticationRoutes);
 };
